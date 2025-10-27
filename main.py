10ed250f9a1ba7edc87381f719ee348df55415b8
McTechDev <thatonefurryreal@gmail.com>
2025-10-27 14:56:51 -0400

    Update main.py

--- main.py
+++ main.py
@@ -19,12 +19,22 @@ def main():
         corners, ids, rejected = aruco.detectMarkers(gray, aruco_dict, parameters=parameters)
 
         if ids is not None:
+            floor_points = []
+
             aruco.drawDetectedMarkers(frame, corners, ids)
             for i, corner in zip(ids, corners):
                 print(f"Detected marker ID: {i[0]}")
-                points = corner[0]
 
-        cv2.line(frame, (x1, y1), (x2, y2), (0, 255, 0), 3)
+                if i[0] == 1 or i[0] == 2 or i[0] == 3 or i[0] == 4:
+                    points = corner[0]
+                    floor_points[i[0]] = points[0]
+
+        for fpnum in range(4):
+            if not fpnum == 3:
+                cv2.line(frame, (floor_points[fpnum][0], floor_points[fpnum][1]), (floor_points[fpnum + 1][0], floor_points[fpnum + 1][1]), (0, 255, 0), 3)
+            else:
+                cv2.line(frame, (floor_points[fpnum][0], floor_points[fpnum][1]), (floor_points[0][0], floor_points[0][1]), (0, 255, 0), 3)
+
         cv2.imshow("ArUco Detection", frame)
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
